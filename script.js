41e89c367787f8918afbe70d150af68134ef8cc0
Zendexx <124657409+Zendexx@users.noreply.github.com>
2023-09-25 00:46:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const openModalButton = document.getElementById('open');
+    const closeModalButton = document.getElementById('close');
+    const modalContainer = document.getElementById('modal-container');
+
+    openModalButton.addEventListener('click', () => {
+        modalContainer.style.display = 'block';
+    });
+
+    closeModalButton.addEventListener('click', () => {
+        modalContainer.style.display = 'none';
+    });
+});
+
+//  the second one
+document.addEventListener('DOMContentLoaded', () => {
+    const openModalButton = document.getElementById('open2');
+    const closeModalButton = document.getElementById('close2');
+    const modalContainer = document.getElementById('modal-container2');
+
+    openModalButton.addEventListener('click', () => {
+        modalContainer.style.display = 'block';
+    });
+
+    closeModalButton.addEventListener('click', () => {
+        modalContainer.style.display = 'none';
+    });
+});
+
+//  the third one
+document.addEventListener('DOMContentLoaded', () => {
+    const openModalButton = document.getElementById('open3');
+    const closeModalButton = document.getElementById('close3');
+    const modalContainer = document.getElementById('modal-container3');
+
+    openModalButton.addEventListener('click', () => {
+        modalContainer.style.display = 'block';
+    });
+
+    closeModalButton.addEventListener('click', () => {
+        modalContainer.style.display = 'none';
+    });
+});
+
+//  the fourth one
+document.addEventListener('DOMContentLoaded', () => {
+    const openModalButton = document.getElementById('open4');
+    const closeModalButton = document.getElementById('close4');
+    const modalContainer = document.getElementById('modal-container4');
+
+    openModalButton.addEventListener('click', () => {
+        modalContainer.style.display = 'block';
+    });
+
+    closeModalButton.addEventListener('click', () => {
+        modalContainer.style.display = 'none';
+    });
+});
